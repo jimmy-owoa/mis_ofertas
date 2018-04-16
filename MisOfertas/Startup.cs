3a83c3f81a961b7371fb13dcfaef3d6224136cc0
Jeremias Gutierrez <jeremiasgutierrez@hotmail.cl>
2018-04-16 01:39:59 -0300

    initialize

---
+++ MisOfertas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MisOfertas.Startup))]
+namespace MisOfertas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
